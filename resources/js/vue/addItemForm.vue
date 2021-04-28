586eae42d4333f452c342c496c2e57a975b4b357
ffayad <ffayad@ptcdb.edu.ps>
2021-04-28 12:40:17 +0300

    start adding components, styling, and install font-awesome

--- resources/js/vue/addItemForm.vue
+++ resources/js/vue/addItemForm.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        Form
+    <div class="addItem">
+        <input type="text"/>
+        <button></button>
     </div>
 </template>
 
--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,10 +1,35 @@
 <template>
-    <div>
-        Hello World
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+            <add-item-form/>
+        </div>
+        <list-view/>
     </div>
 </template>
 <script>
+import AddItemForm from './addItemForm'
+import ListView from './listView.vue'
 export default {
-
+    components: {
+        AddItemForm,
+        ListView
+    }
 }
 </script>
+
+<style scooped>
+    .todoListContainer {
+        width: 350px;
+        margin: auto;
+    }
+
+    .heading {
+        background: #e6e6e6;
+        padding: 10px;
+    }
+
+    #title{
+        text-align: center;
+    }
+</style>
