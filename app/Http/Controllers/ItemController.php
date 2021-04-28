f4e8ff89e5833d40a7380ab834bc3a7176832b7f
ffayad <ffayad@ptcdb.edu.ps>
2021-04-28 11:49:00 +0300

    controller delete  function

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -93,6 +93,14 @@ class ItemController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $existingItem = Item::find($id);
+        if ($existingItem) {
+
+            $existingItem->delete();
+
+            return "Item successfully deleted.";
+        }
+
+        return "Item not found.";
     }
 }
